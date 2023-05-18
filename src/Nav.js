b67089c4a11473915bb7af66569346f19b7802fd
TakehiroTsukagoshi <takehiro.thanks@gmail.com>
2023-05-18 21:44:30 +0900

    navi

---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css"
+
+function Nav() {
+
+  const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    }
+  }, [])
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="Netflix Logo"/>
+      <img className="nav__avatar" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="Netflix Logo"/>
+    </div>
+  )
+}
+
+export default Nav
